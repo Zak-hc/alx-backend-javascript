eaa2b401bd5047156bfac4c86a3b3c383ca51559
Zak-hc <zhaci001@gmail.com>
2024-03-07 23:02:27 +0000

    good_jb

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,54 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError('length must be a number');
+    }
+    if (Array.isArray(students)) {
+      this._students = students;
+    } else {
+      throw new TypeError('students must be an array of sstrings');
+    }
+  }
+ get name() {
+    return this._name;
+  }
+
+  set name(a) {
+    if (typeof a === 'string') {
+      this._name = a;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(b) {
+    if (typeof b === 'number') {
+      this._length = b;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(c) {
+    if (Array.isArray(c) && c.every(e => typeof e === 'string')) {
+      this._students = c;
+    } else {
+      throw new TypeError('Students must be an array of strings');
+    }
+  }
+}
