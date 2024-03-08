5f928d8dcd87ce6664ab0fd2720bed619d79650a
Zak-hc <zhaci001@gmail.com>
2024-03-08 17:30:03 +0000

    alae is agood liar

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,22 +1,28 @@
 import Building from './5-building';
+
 export default class kyHighBuilding extends Building {
-constructor(sqft ,floors) {
-super(sqft);
-this._floors = floors;
-}
-get sqft() {
-return this._sqft;
-}
-set sqft(a) {
-this._sqft = a;
-}
-get floors() {
-return this._floors;
-}
-set floors(b) {
-this._floors = b;
-}
-evacuationWarningMessage() {
-return `Evacuate slowly the ${this._floors} floors`;
-}
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(a) {
+    this._sqft = a;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(b) {
+    this._floors = b;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
 }
