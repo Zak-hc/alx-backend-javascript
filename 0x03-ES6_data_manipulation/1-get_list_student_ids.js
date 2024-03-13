931eda1fed3e046463c2e3385cc4a276db7475b8
Zak-hc <zhaci001@gmail.com>
2024-03-13 00:06:21 +0000

    hlp by outh

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,6 +2,8 @@ export default function getListStudentIds(tkhwera) {
   if (!Array.isArray(tkhwera)) {
     return [];
   }
-  const new_tab = tkhwera.map((t1) => t1.id);
-  return new_tab;
+
+  const newtab = tkhwera.map((t1) => t1.id);
+
+  return newtab;
 }
